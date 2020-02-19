becb20ff3c56981d17bd9fa6cce77a928b8ae52c
Jackson Tubbs <decknot@jacksons-mbp.attlocal.net>
2020-02-18 19:30:07 -0800

    First save

---
+++ Core Graphics 1/Resources/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Core Graphics 1
+//
+//  Created by Jackson Tubbs on 2/11/20.
+//  Copyright © 2020 Jackson Tubbs. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Properties
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        presentInitialViewController()
+        return true
+    }
+
+    private func presentInitialViewController() {
+        let initialViewController = MainViewController(nibName: nil, bundle: nil)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
