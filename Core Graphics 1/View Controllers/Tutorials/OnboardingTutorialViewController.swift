0a6a66ccc57b73686cf3074a0775bb8792a45e0a
Jackson Tubbs <decknot@jacksons-mbp.attlocal.net>
2020-03-25 17:38:38 -0700

    Adds paging

--- Core Graphics 1/View Controllers/Tutorials/OnboardingTutorialViewController.swift
+++ Core Graphics 1/View Controllers/Tutorials/OnboardingTutorialViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class OnboardingTutorialViewController: UIViewController {
 
+    // MARK: Properties
+    
+    let numberOfPages = 3
+    var pageOriginPoints: [CGFloat] = []
+    
     // MARK: Views
     
-    let numberOfPages = 2
+    var parentViews: [UIView] = []
     
-    // MARK: Styleguide
+    // MARK: Style Guide
 
     // MARK: Init
     
@@ -33,6 +38,15 @@ class OnboardingTutorialViewController: UIViewController {
         setupViews()
     }
     
+    // MARK: Other Overrides
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        for page in 0..<numberOfPages {
+//            print("Page: \(page) Frame: \(parentViews[page].frame)")
+//        }
+//    }
+    
     // MARK: Helpers
     
     // MARK: Setup Views
@@ -40,11 +54,13 @@ class OnboardingTutorialViewController: UIViewController {
         self.view.backgroundColor = .backgroundColor
         
         let scrollView = UIScrollView()
+        scrollView.delegate = self
+        scrollView.decelerationRate = .fast
         scrollView.alwaysBounceHorizontal = true
         scrollView.alwaysBounceVertical = false
         scrollView.backgroundColor = .blue
         scrollView.showsVerticalScrollIndicator = false
-        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = true
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(scrollView)
         NSLayoutConstraint.activate([
@@ -56,17 +72,64 @@ class OnboardingTutorialViewController: UIViewController {
         
         for page in 0..<numberOfPages {
             let parentView = UIView()
-            parentView.backgroundColor = .red
+            parentView.backgroundColor = .backgroundColor
             parentView.translatesAutoresizingMaskIntoConstraints = false
             scrollView.addSubview(parentView)
+            if page == 0 {
+                parentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+            } else {
+                parentView.leadingAnchor.constraint(equalTo: parentViews[page - 1].trailingAnchor).isActive = true
+                if page == numberOfPages - 1 {
+                    parentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+                }
+            }
             NSLayoutConstraint.activate([
-                parentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-                parentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
                 parentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
                 parentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
                 parentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
                 parentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
             ])
+            parentViews.append(parentView)
+            pageOriginPoints.append(CGFloat(page) * self.view.frame.width + self.view.frame.width / 2)
+            
+            let dot = UIView()
+            dot.backgroundColor = .black
+            dot.translatesAutoresizingMaskIntoConstraints = false
+            parentView.addSubview(dot)
+            NSLayoutConstraint.activate([
+                dot.centerYAnchor.constraint(equalTo: parentView.centerYAnchor),
+                dot.centerXAnchor.constraint(equalTo: parentView.centerXAnchor),
+                dot.heightAnchor.constraint(equalToConstant: 30),
+                dot.widthAnchor.constraint(equalToConstant: 30)
+            ])
+        }
+    }
+}
+
+extension OnboardingTutorialViewController: UIScrollViewDelegate {
+    
+    // Will set the target Content Offset to be equal to a cards origin position
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        var closestPoint: CGFloat = CGFloat.infinity
+        print("\(targetContentOffset.pointee)")
+        for index in 0..<pageOriginPoints.count {
+            print("Page Origin POints: \(pageOriginPoints)")
+            var distanceToOrigin = targetContentOffset.pointee.x + self.view.bounds.width / 2 - pageOriginPoints[index]
+            if distanceToOrigin < 0 {
+                distanceToOrigin *= -1
+            }
+            if distanceToOrigin  < closestPoint {
+                closestPoint = distanceToOrigin
+                if index + 1 == pageOriginPoints.count {
+                    targetContentOffset.pointee = CGPoint(x: pageOriginPoints[index] - self.view.frame.width / 2, y: 0)
+                }
+            } else if index == 0 {
+                targetContentOffset.pointee = CGPoint(x: pageOriginPoints[index] - self.view.frame.width / 2, y: 0)
+                return
+            } else {
+                targetContentOffset.pointee = CGPoint(x: pageOriginPoints[index - 1] - self.view.frame.width / 2, y: 0)
+                return
+            }
         }
     }
 }
